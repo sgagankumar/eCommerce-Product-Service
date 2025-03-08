7109b0e7b26d11bb45372183f60016751095ca2d
Developer 1 <you@example.com>
2025-03-08 21:05:41 +0530

    Added initial project files and dependencies

---
+++ src/test/java/com/sgagankumar/productservice/ProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgagankumar.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
