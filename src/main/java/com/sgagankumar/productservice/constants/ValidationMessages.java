07f899e7735fcb284ae4144ede3017e1e2e52e01
sgagankumar <sgagankumar@gmail.com>
2025-03-18 23:30:07 +0530

    Added CreateProduct API Implementation

---
+++ src/main/java/com/sgagankumar/productservice/constants/ValidationMessages.java
@@ -0,0 +1,10 @@
+package com.sgagankumar.productservice.constants;
+
+public class ValidationMessages
+{
+    public static final String NAME_REQUIRED  = "Name cannot be empty.";
+    public static final String PRICE_REQUIRED = "Price cannot be null.";
+    public static final String PRICE_INVALID = "Price must be greater than 0.";
+    public static final String IMAGE_URL_INVALID = "Invalid image URL format.";
+    public static final String CATEGORY_NAME_REQUIRED = "Category name cannot be empty.";
+}
--- src/main/java/com/sgagankumar/productservice/services/ProductServiceImpl.java
+++ src/main/java/com/sgagankumar/productservice/services/ProductServiceImpl.java
@@ -1,19 +1,27 @@
 package com.sgagankumar.productservice.services;
 
 import com.sgagankumar.productservice.models.Product;
+import com.sgagankumar.productservice.models.Category;
+import com.sgagankumar.productservice.repositories.CategoryRepository;
 import com.sgagankumar.productservice.repositories.ProductRepository;
 import com.sgagankumar.productservice.exceptions.ProductNotFoundException;
 import org.springframework.stereotype.Service;
 
 
+import java.time.Instant;
+import java.util.Date;
 import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService
 {
     ProductRepository productRepository;
+    CategoryRepository categoryRepository;
 
-    public ProductServiceImpl(ProductRepository productRepository) {this.productRepository = productRepository;}
+    public ProductServiceImpl(ProductRepository productRepository, CategoryRepository categoryRepository) {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
 
     @Override
     public Product getProductById(long id) throws ProductNotFoundException{
@@ -22,5 +30,31 @@ public class ProductServiceImpl implements ProductService
             throw new ProductNotFoundException("No details present for the product id : "+ id);
         }
         return product.get();
+    }
+
+    @Override
+    public Product createProduct(String name, String description, double price, String imageurl, String category) {
+        Product product = new Product();
+        product.setName(name);
+        product.setDescription(description);
+        product.setPrice(price);
+        product.setImageUrl(imageurl);
+        product.setActive(true);
+        product.setCreatedAt(Date.from(Instant.now()));
+        product.setUpdatedAt(Date.from(Instant.now()));
+        product.setCategory(getCategory(category));
+        return productRepository.save(product);
+    }
+
+    private Category getCategory(String categoryName){
+        Optional<Category> category = categoryRepository.findByName(categoryName);
+        if(category.isPresent() && category.get().isActive())
+            return category.get();
+        Category newCategory = new Category();
+        newCategory.setName(categoryName);
+        newCategory.setActive(true);
+        newCategory.setCreatedAt(Date.from(Instant.now()));
+        newCategory.setUpdatedAt(Date.from(Instant.now()));
+        return categoryRepository.save(newCategory);
     }
 }
